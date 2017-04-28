441c89c12cfed0c1ac27e54fa0caa5f9ed2bd57c
olivergoodman <olivergoodman2017@u.northwestern.edu>
2017-04-28 13:50:32 -0500

    experimenting with gest.js

---
+++ static/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	gest.options.debug(true); //show gesture video feed
+
+
+	var slide = document.getElementById('gest-slider'),
+	    sliderText = document.getElementById("gest-sensitivity");
+
+	slide.onchange = function() {
+	    sliderText.innerHTML = this.value;
+	    gest.options.sensitivity(slide.valueAsNumber);
+	}
+
+
+	$("#gest-start").click(function() {
+		
+		gest.start();
+
+		gest.options.subscribeWithCallback(function(gesture) {
+			$("#direction-log")[0].innerHTML = gesture.direction;
+
+			// can handle individual directions:
+		    if (gesture.up) {
+		    	console.log(gesture.direction);
+		    } else if (gesture.down) {
+		    	console.log(gesture.direction);
+		    } else if (gesture.left) {
+		    	console.log(gesture.direction);
+		    } else if (gesture.right) {
+		    	console.log(gesture.direction);
+		    } else {
+		    	console.log(gesture.error);
+		    }
+		});	
+	});
+
+	$("#gest-stop").click(function() {
+		gest.stop();
+	});
+	
+
+	
+
+
+})
