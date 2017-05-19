f3fafda7c1121499cba88391968b20cba5ad72b7
olivergoodman <olivergoodman2017@u.northwestern.edu>
2017-05-19 13:47:26 -0500

    sending blob to python backend, but may need to switch to node

---
+++ run.py
@@ -0,0 +1,39 @@
+import SimpleHTTPServer
+import SocketServer
+import logging
+import cgi
+import json
+
+PORT = 8000
+
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        # logging.error(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        # logging.error(self.headers)
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+        for item in form.list:
+        	for key in form.keys():
+				variable = str(key)
+				blob = form.getvalue(variable);
+				print len(blob)
+				# value = json.loads(str(form.getvalue(variable)))
+    	        # file_name = value["name"]
+    	        # chunks = value["chunks"]
+
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+Handler = ServerHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
--- static/js/recording.js
+++ static/js/recording.js
@@ -1,60 +1,69 @@
 $(document).ready(function() {
-	// The number of gesture snippets to be recorded
-	var MAX_NUMBER_GESTURES = 4;
+	// jQuery
+	$.getScript('../static/js/record_video.js', function()
+	{
+    	// The number of gesture snippets to be recorded
+    	var MAX_NUMBER_GESTURES = 4;
 
-	// The current count for the number of gesture snippets recorded
-	var COUNT = 0;
+    	// The current count for the number of gesture snippets recorded
+    	var COUNT = 0;
 
-	// Keep track if we are at the first press (start recording) or 2nd (end recording)
-	var FIRST_PRESS = false;
+    	// Keep track if we are at the first press (start recording) or 2nd (end recording)
+    	var FIRST_PRESS = false;
 
-	// The different directions that will be recorded
-	var DIRECTIONS = ["up", "down", "left", "right", "up", "down", "left", "right"]
-	// and set the first direction
-	$("#modal-dir").text(DIRECTIONS[0]);
+    	// The different directions that will be recorded
+    	var DIRECTIONS = ["up", "down", "left", "right", "up", "down", "left", "right"]
+    	// and set the first direction
+    	$("#modal-dir").text(DIRECTIONS[0]);
 
-	// Get the modal
-	var modal = document.getElementById('myModal');
+    	// Get the modal
+    	var modal = document.getElementById('myModal');
 
-	// Get the button that opens the modal
-	var btn = document.getElementById("myBtn");
+    	// Get the button that opens the modal
+    	var btn = document.getElementById("myBtn");
 
-	// Get the <span> element that closes the modal
-	var span = document.getElementsByClassName("close")[0];
+    	// Get the <span> element that closes the modal
+    	var span = document.getElementsByClassName("close")[0];
 
-	// When the user clicks the button, open the modal 
-    modal.style.display = "block";
+    	// When the user clicks the button, open the modal 
+        modal.style.display = "block";
 
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	    modal.style.display = "none";
-	}
+    	// When the user clicks on <span> (x), close the modal
+    	span.onclick = function() {
+    	    modal.style.display = "none";
+    	}
 
-	// Recognize key presses
-	document.onkeypress = function(evt) {
-	    evt = evt || window.event;
-	    var charCode = evt.keyCode || evt.which;
-	    if (charCode == 32 && COUNT < MAX_NUMBER_GESTURES) {
-	    	FIRST_PRESS = !FIRST_PRESS;
-	    	
-	    	//start recording:
-	    	if (FIRST_PRESS == true) {
-	    		console.log('start recording');
-	    	}
+    	// zip file of vid snippets to download
+    	var zip = new JSZip();
 
-	    	// end recording:
-	    	else {
-	    		COUNT++;
-	    		// when space is pressed load next instruction
-				console.log("finished recording", COUNT);	
-				$("#modal-dir").text(DIRECTIONS[COUNT]);
-	    	}
-	    }
+    	// Recognize key presses
+    	document.onkeypress = function(evt) {
+    	    evt = evt || window.event;
+    	    var charCode = evt.keyCode || evt.which;
+    	    if (charCode == 32 && COUNT < MAX_NUMBER_GESTURES) {
+    	    	FIRST_PRESS = !FIRST_PRESS;
+    	    	
+    	    	//start recording:
+    	    	if (FIRST_PRESS == true) {
+    	    		console.log('start recording');
+    	    		onBtnRecordClicked();
+    	    	}
 
-	    // close modals after reached MAX_NUMBER_GESTURES
-	    if (COUNT >= MAX_NUMBER_GESTURES) {
-	    	modal.style.display = "none";
-	    }
-	    
-	};
+    	    	// end recording:
+    	    	else {
+    	    		COUNT++;
+    	    		// when space is pressed load next instruction
+    	    		onBtnStopClicked();
+
+    				$("#modal-dir").text(DIRECTIONS[COUNT]);
+    				console.log("finished recording", COUNT);
+    	    	}
+    	    }
+
+    	    // close modals after reached MAX_NUMBER_GESTURES
+    	    if (COUNT >= MAX_NUMBER_GESTURES) {
+    	    	modal.style.display = "none";
+    	    }  
+    	};
+	});
 });
