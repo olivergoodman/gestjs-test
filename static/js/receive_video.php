c60e9c83f26127a924118dca38d4e1db977e71fb
olivergoodman <olivergoodman2017@u.northwestern.edu>
2017-05-19 13:48:05 -0500

    remove .php

--- static/js/receive_video.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-if(isset($_FILES['file']) and !$_FILES['file']['error']){
-    $fname = "11" . ".webm";
-
-    move_uploaded_file($_FILES['file']['tmp_name'], "../videos/" . $fname);
-}
-?>
